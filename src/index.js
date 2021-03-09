1eaf045b59cb0b9c8bfed8d44795afa3acd6042f
Wahab Shaikh <wahab.shaikh@somaiya.edu>
2021-03-10 00:50:32 +0530

    🌴 03-JSX

--- src/index.js
+++ src/index.js
@@ -1,35 +1,34 @@
 const Movie = (props) => {
-  return React.createElement("div", {}, [
-    React.createElement("h1", {}, props.title),
-    React.createElement("small", {}, props.release_date),
-    React.createElement("img", {
-      src: props.poster_path,
-    }),
-  ]);
+  return (
+    <div>
+      <h1>{props.title}</h1>
+      <small>{props.release_date}</small>
+      <img src={props.poster_path} />
+    </div>
+  );
 };
 
 const App = () => {
-  return React.createElement("div", {}, [
-    React.createElement("h1", {}, "React Workshop"),
-    React.createElement(Movie, {
-      title: "The Dark Knight Rises",
-      release_date: "2012-07-20",
-      poster_path:
-        "https://m.media-amazon.com/images/M/MV5BMTk4ODQzNDY3Ml5BMl5BanBnXkFtZTcwODA0NTM4Nw@@._V1_UX182_CR0,0,182,268_AL__QL50.jpg",
-    }),
-    React.createElement(Movie, {
-      title: "Interstellar",
-      release_date: "2014-11-07",
-      poster_path:
-        "https://m.media-amazon.com/images/M/MV5BZjdkOTU3MDktN2IxOS00OGEyLWFmMjktY2FiMmZkNWIyODZiXkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_UX182_CR0,0,182,268_AL__QL50.jpg",
-    }),
-    React.createElement(Movie, {
-      title: "Inception",
-      release_date: "2010-07-16",
-      poster_path:
-        "https://m.media-amazon.com/images/M/MV5BMjAxMzY3NjcxNF5BMl5BanBnXkFtZTcwNTI5OTM0Mw@@._V1_UX182_CR0,0,182,268_AL__QL50.jpg",
-    }),
-  ]);
+  return (
+    <div>
+      <h1>React Workshop</h1>
+      <Movie
+        title="The Dark Knight"
+        release_date="2012-07-20"
+        poster_path="https://m.media-amazon.com/images/M/MV5BMTk4ODQzNDY3Ml5BMl5BanBnXkFtZTcwODA0NTM4Nw@@._V1_UX182_CR0,0,182,268_AL__QL50.jpg"
+      />
+      <Movie
+        title="Interstellar"
+        release_date="2014-11-07"
+        poster_path="https://m.media-amazon.com/images/M/MV5BZjdkOTU3MDktN2IxOS00OGEyLWFmMjktY2FiMmZkNWIyODZiXkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_UX182_CR0,0,182,268_AL__QL50.jpg"
+      />
+      <Movie
+        title="Inception"
+        release_date="2010-07-16"
+        poster_path="https://m.media-amazon.com/images/M/MV5BMjAxMzY3NjcxNF5BMl5BanBnXkFtZTcwNTI5OTM0Mw@@._V1_UX182_CR0,0,182,268_AL__QL50.jpg"
+      />
+    </div>
+  );
 };
 
-ReactDOM.render(React.createElement(App), document.getElementById("root"));
+ReactDOM.render(<App />, document.getElementById("root"));
