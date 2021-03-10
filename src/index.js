02825d5b2bb342daa8454c712b7eedad86b44494
Wahab Shaikh <wahab.shaikh@somaiya.edu>
2021-03-10 16:36:53 +0530

    :hammer_and_wrench: Remove release_date prop & change title to h2

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
 const Movie = (props) => {
   return (
     <div>
-      <h1>{props.title}</h1>
-      <small>{props.release_date}</small>
+      <h2>{props.title}</h2>
       <img src={props.poster_path} />
     </div>
   );
@@ -14,17 +13,14 @@ const App = () => {
       <h1>React Workshop</h1>
       <Movie
         title="The Dark Knight"
-        release_date="2012-07-20"
         poster_path="https://m.media-amazon.com/images/M/MV5BMTk4ODQzNDY3Ml5BMl5BanBnXkFtZTcwODA0NTM4Nw@@._V1_UX182_CR0,0,182,268_AL__QL50.jpg"
       />
       <Movie
         title="Interstellar"
-        release_date="2014-11-07"
         poster_path="https://m.media-amazon.com/images/M/MV5BZjdkOTU3MDktN2IxOS00OGEyLWFmMjktY2FiMmZkNWIyODZiXkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_UX182_CR0,0,182,268_AL__QL50.jpg"
       />
       <Movie
         title="Inception"
-        release_date="2010-07-16"
         poster_path="https://m.media-amazon.com/images/M/MV5BMjAxMzY3NjcxNF5BMl5BanBnXkFtZTcwNTI5OTM0Mw@@._V1_UX182_CR0,0,182,268_AL__QL50.jpg"
       />
     </div>
