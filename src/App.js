e87dc4b3dc3443cd3282b367272793e147c0cb72
Wahab Shaikh <wahab.shaikh@somaiya.edu>
2021-03-13 17:10:56 +0530

    📓 07-Conditional-Rendering

--- src/App.js
+++ src/App.js
@@ -1,14 +1,6 @@
 import Banner from "./Banner";
-import Category from "./Category";
 import Search from "./Search";
 import requests from "./requests";
-
-const categories = [
-  { title: "Now Playing", fetchUrl: requests.fetchNowPlaying },
-  { title: "Popular", fetchUrl: requests.fetchPopular },
-  { title: "Top Rated", fetchUrl: requests.fetchTopRated },
-  { title: "Upcoming", fetchUrl: requests.fetchUpcoming },
-];
 
 const App = () => {
   return (
@@ -17,13 +9,6 @@ const App = () => {
       <Search />
 
       <Banner fetchUrl={requests.fetchTrending} />
-      {categories.map((category) => (
-        <Category
-          key={category.title}
-          title={category.title}
-          fetchUrl={category.fetchUrl}
-        />
-      ))}
     </div>
   );
 };
